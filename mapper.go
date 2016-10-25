63ea66aff8e2c801ddec3eb656f2c14ff31979f3
Jonathan Summer <jonathan.summer@guggenheimpartners.com>
2016-10-25 17:51:41 -0400

    two implementations

--- mapper.go
+++ mapper.go
@@ -11,17 +11,24 @@ import (
 	"github.com/smartystreets/mafsa"
 )
 
-// Mapper maps string keys to string values with a low memory footprint.  It should be suitable for
+// MafsaMapper maps string keys to string values with a low memory footprint.  It should be suitable for
 // very large datasets
-type Mapper struct {
+type MafsaMapper struct {
 	mt   *mafsa.MinTree
 	vals []string
 	From string
 	To   string
 }
 
+// MapMapper just uses a map.  Let's see if it uses more memory.
+type MapMapper struct {
+	m    map[string]string
+	From string
+	To   string
+}
+
 // Get returns a value for a key, and a boolean indicating whether the key was found.
-func (m Mapper) Get(key string) (value string, ok bool) {
+func (m MafsaMapper) Get(key string) (value string, ok bool) {
 	_, pos := m.mt.IndexedTraverse([]rune(key))
 	if pos < 0 {
 		return // returns zero values for value ("") and ok (false)
@@ -31,12 +38,18 @@ func (m Mapper) Get(key string) (value string, ok bool) {
 	return
 }
 
-// NewMapperFromCSV takes an io.Reader of CSV data and returns a Mapper ready for use.
+// Get returns a value for a key, and a boolean indicating whether the key was found.
+func (m MapMapper) Get(key string) (value string, ok bool) {
+	value, ok = m.m[key]
+	return
+}
+
+// NewMafsa takes an io.Reader of CSV data and returns a MafsaMapper ready for use.
 // The CSV data must be in UTF-8 and have only two fields with a header row. The resulting
 // Mapper will map values from the first column to values in the second column.  The data
 // after the header row must be sorted lexographically by the first column.
-func New(csvData io.Reader) (m *Mapper, err error) {
-	m = &Mapper{}
+func NewMafsa(csvData io.Reader) (m *MafsaMapper, err error) {
+	m = &MafsaMapper{}
 	r := csv.NewReader(csvData)
 	r.LazyQuotes = true
 	r.FieldsPerRecord = 2
@@ -87,5 +100,37 @@ func New(csvData io.Reader) (m *Mapper, err error) {
 	// release the memory for the BuildTree before we load the MinTree
 	debug.FreeOSMemory()
 	m.mt, err = new(mafsa.Decoder).ReadFrom(tmp)
+	return
+}
+
+// NewMap takes an io.Reader of CSV data and returns a Mapper ready for use.
+// The CSV data must be in UTF-8 and have only two fields with a header row. The resulting
+// Mapper will map values from the first column to values in the second column.  The data
+// after the header row must be sorted lexographically by the first column.
+func NewMap(csvData io.Reader) (m *MapMapper, err error) {
+	m = &MapMapper{}
+	r := csv.NewReader(csvData)
+	r.LazyQuotes = true
+	r.FieldsPerRecord = 2
+	var row []string
+	row, err = r.Read()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	m.From = row[0]
+	m.To = row[1]
+	m.m = make(map[string]string)
+	i := 0
+	for {
+		i++
+		row, err = r.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return
+		}
+		m.m[row[0]] = row[1]
+	}
 	return
 }
